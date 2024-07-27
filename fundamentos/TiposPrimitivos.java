8da030f634d213bd680c29fdee8b5bc6f80f7c5f
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2024-07-27 16:16:07 -0300

    Tipos primitivos do java

---
+++ fundamentos/TiposPrimitivos.java
@@ -0,0 +1,20 @@
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		byte idade = 25;
+		short ano = 2024;
+		int populacaoCidade = 35000;
+		long populacaoMundial = 800000000;
+		float altura = 1.65f;
+		double salario = 2500.00;
+		boolean estudante = true;
+		char inicialNome = 'R';
+
+		System.out.println("Idade: " + idade);
+		System.out.println("Ano atual: " + ano);
+		System.out.println("População da cidade: " + populacaoCidade);
+		System.out.println("Altura: " + altura);
+		System.out.println("Salário: " + salario);
+		System.out.println("Estudante: " + estudante);
+		System.out.println("Inicial do nome: " + inicialNome);
+	}
+}
