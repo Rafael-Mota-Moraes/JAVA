9f15f53ebf15b5d28348e621c25a592409a26ca8
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2024-07-27 16:36:47 -0300

    Comentários

---
+++ fundamentos/Comentarios.java
@@ -0,0 +1,15 @@
+class Comentarios {
+	public static void main(String[] args) {
+		// Comentário de linha única
+		/*
+		Comentário
+		de
+		várias
+		linhas...
+		*/
+
+		/**
+		javadoc
+		*/
+		}
+}
