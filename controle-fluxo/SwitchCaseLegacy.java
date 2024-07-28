db8081be94ff974c3644303b9e3093f7e7681d65
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2024-07-28 14:23:33 -0300

    switch-case legacy

---
+++ controle-fluxo/SwitchCaseLegacy.java
@@ -0,0 +1,20 @@
+class SwitchCaseLegacy {
+	public static void main(String[] args) {
+		int diaDaSemana = 3;
+
+	switch(diaDaSemana) {
+		case 1:
+			System.out.println("Segunda");
+			break;
+		case 2:
+			System.out.println("Terça");
+			break;
+		case 3:
+			System.out.println("Quarta");
+			break;
+		default:
+			System.out.println("Dia desconhecido");
+			break;
+	}
+	}
+}
