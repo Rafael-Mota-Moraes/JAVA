58356a461101f7c628dc9c57fce0294beb1b5c9e
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2024-07-28 11:31:01 -0300

    operadores lógicos

---
+++ operadores/Logicos.java
@@ -0,0 +1,11 @@
+class Logicos {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		boolean c = true;
+
+		System.out.println(a < b && c);
+		System.out.println(a > b || c);
+		System.out.println(!c);
+	}
+}
