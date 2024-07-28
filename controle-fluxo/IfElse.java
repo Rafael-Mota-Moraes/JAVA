f00ff523be649bbd68bc24bf6898a6398f839393
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2024-07-28 14:02:48 -0300

    if else

---
+++ controle-fluxo/IfElse.java
@@ -0,0 +1,11 @@
+class IfElse {
+	public static void main(String[] args) {
+		int a = 10;
+		
+		if (a > 5) {
+			System.out.println("a é maior que 5");
+		} else {
+			System.out.println("a é menor que 5");
+		}
+	}
+}
