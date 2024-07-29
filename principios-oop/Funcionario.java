b76bea4b787b4d354fcb53782f1876a0056e589b
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2024-07-29 15:52:42 -0300

    Construtor default

--- principios-oop/Funcionario.java
+++ principios-oop/Funcionario.java
@@ -3,8 +3,11 @@ class Funcionario {
 	String cargo;
 	int idade;
 
+	
+	// Construtor default
+	Funcionario() {} 
+	
 	// Este é o construtor
-
 	Funcionario(String nome, String cargo, int idade) {
 		this.nome = nome;
 		this.cargo = cargo;
@@ -23,6 +26,15 @@ class Funcionario {
 		System.out.println("Nome: " + func2.nome);
 		System.out.println("Cargo: " + func2.cargo);
 		System.out.println("Idade: " + func2.idade);
+
+		Funcionario func3 = new Funcionario(); // Usando o construtor padrão
+		func3.nome = "Pedro";
+		func3.cargo = "Junior";
+		func3.idade = 17;
+		System.out.println();
+		System.out.println("Nome: " + func3.nome);
+		System.out.println("Cargo: " + func3.cargo);
+		System.out.println("Idade: " + func3.idade);
 
 	}
 }
